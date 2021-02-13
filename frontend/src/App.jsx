42d701b73e42ead9efb801e96cfa9c006f15dfff
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-12 20:44:13 -0600

    8. Implementing React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<Fragment>
+		<Router>
 			<Header />
 			<main className='py-3'>
 				<Container>
-					<HomeScreen />
+					<Route path='/' component={HomeScreen} exact />
+					<Route path='/product/:id' component={ProductScreen} />
 				</Container>
 			</main>
 			<Footer />
-		</Fragment>
+		</Router>
 	);
 }
 
