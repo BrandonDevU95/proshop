0d2e6badf0e233d09c9f4f2fddc8fee30b8ef3fe
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-17 23:21:35 -0600

    29. Message & Loader Components

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -3,6 +3,8 @@ import { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Product from '../Components/Product';
 import { listProducts } from '../actions/productActions';
+import Message from '../Components/Message';
+import Loader from '../Components/Loader';
 
 const HomeScreen = () => {
 	const dispatch = useDispatch();
@@ -18,9 +20,9 @@ const HomeScreen = () => {
 		<Fragment>
 			<h1>Latest Products</h1>
 			{loading ? (
-				<h2>Loading...</h2>
+				<Loader />
 			) : error ? (
-				<h3>{error} </h3>
+				<Message variant='danger'>{error}</Message>
 			) : (
 				<Row>
 					{products.map((product) => (
