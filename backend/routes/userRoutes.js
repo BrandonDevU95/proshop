73d6e3fe84f7bef5f965d00e0a746cd5482f7938
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-19 16:30:33 -0600

    40. Custom Authentication Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUersProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUersProfile);
 
 export default router;
