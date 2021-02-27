8babb9f183c7439c0d96db99806eae97b8f7b3ef
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-26 19:28:27 -0600

    48. Profile Screen & Get User Details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import ProductScreen from './Screens/ProductScreen';
 import CartScreen from './Screens/CartScreen';
 import LoginScreen from './Screens/LoginScreen';
 import RegisterScreen from './Screens/RegisterScreen';
+import ProfileScreen from './Screens/ProfileScreen';
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 			<Header />
 			<main className='py-3'>
 				<Container>
+					<Route path='/profile' component={ProfileScreen} />
 					<Route path='/register' component={RegisterScreen} />
 					<Route path='/login' component={LoginScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
